d1300bbcfe6899a70d307429d7ea97a310f2ee8e
Pavel Stoffel <pstoffel@slac.stanford.edu>
2011-02-16 00:48:56 +0000

    Startup files for two IOC's supporting one camera each
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@5009 ececa60f-9f63-0410-97c7-ea05888eb260

--- iocBoot/ioc-xpp-gige-cam2/st.cmd
+++ iocBoot/ioc-xpp-gige-cam2/st.cmd
@@ -1,23 +1,10 @@
 #!../../bin/linux-x86_64/gige
 
-## ToDo: The following substitutions can be done via makeBaseApp.pl
-## If they weren't, do them before releasing your IOC
-##
-## Replace _ USER _ with your userid
-##
-## Replace _ APPNAME _ with the name of the application
-##
-## Replace _ IOC _ with the network name of the IOC
-##
-## Replace _ IOCPVROOT _ with the PV prefix used for
-## iocAdmin PV's on this IOC
-## ex. AMO:R15:IOC:23
-##
-
-epicsEnvSet( "ENGINEER", "Pavel Stoffel" )
-epicsEnvSet( "LOCATION", "XPP:R37:IOC:14:GIGE:02" )
-epicsEnvSet( "IOCSH_PS1", "ioc-xpp-gige-cam2> " )
 < envPaths
+epicsEnvSet( "ENGINEER", "Pavel Stoffel" )
+epicsEnvSet( "LOCATION", "XPP:R37:IOC:14" )
+epicsEnvSet( "IOC", "ioc-xpp-gige-cam2" )
+epicsEnvSet( "IOCSH_PS1", "$(IOC)> " )
 cd( "../.." )
 
 # Run common startup commands for linux soft IOC's
@@ -27,24 +14,66 @@ cd( "../.." )
 dbLoadDatabase("dbd/gige.dbd")
 gige_registerRecordDeviceDriver(pdbbase)
 
+
+##############################################################
+# configure and initialize the camera
+#   Args:  port, dummy, ip, nbufers, nbufers x width x height + overhead
+prosilicaConfigIp(  "PS2", 116474, "172.21.38.67", 50, 70000000)
+##############################################################
+
 # Load record instances
-dbLoadRecords( "db/iocAdmin.db",			"IOC=XPP:IOC:GIGE:02" )
-dbLoadRecords( "db/save_restoreStatus.db",	"IOC=XPP:IOC:GIGE:02" )
+dbLoadRecords( "db/iocAdmin.db",			"IOC=XPP:USR:IOC:GIGE2" )
+dbLoadRecords( "db/save_restoreStatus.db",	"IOC=XPP:USR:IOC:GIGE2" )
+#
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=XPP:USR:GIGE2:,R=cam1:,PORT=PS2,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=XPP:USR:GIGE2:,R=cam1:,PORT=PS2,ADDR=0,TIMEOUT=1")
+#
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+drvNDStdArraysConfigure("PS2Image", 5, 0, "PS2", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=XPP:USR:GIGE2:,R=image1:,PORT=PS2Image,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS2,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=XPP:USR:GIGE2:,R=image1:,PORT=PS2Image,ADDR=0,TIMEOUT=1,SIZE=8,FTVL=UCHAR,NELEMENTS=1392640")
+#
+# Create a file saving plugin
+drvNDFileConfigure("PS2File", 5, 0, "PS2", 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=XPP:USR:GIGE2:,R=file1:,PORT=PS2File,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS2,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",      "P=XPP:USR:GIGE2:,R=file1:,PORT=PS2File,ADDR=0,TIMEOUT=1")
+#
+# Create an ROI plugin
+drvNDROIConfigure("PS2ROI", 5, 0, "PS2", 0, 10, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=XPP:USR:GIGE2:,R=ROI1:,  PORT=PS2ROI,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS2,NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROI.template",       "P=XPP:USR:GIGE2:,R=ROI1:,  PORT=PS2ROI,ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=XPP:USR:GIGE2:,R=ROI1:0:,PORT=PS2ROI,ADDR=0,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=XPP:USR:GIGE2:,R=ROI1:1:,PORT=PS2ROI,ADDR=1,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=XPP:USR:GIGE2:,R=ROI1:2:,PORT=PS2ROI,ADDR=2,TIMEOUT=1,HIST_SIZE=256")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDROIN.template",      "P=XPP:USR:GIGE2:,R=ROI1:3:,PORT=PS2ROI,ADDR=3,TIMEOUT=1,HIST_SIZE=256")
+#
+# Load a prosilica gigE camera
+#dbLoadRecords( "db/gigE.db",      "P=XPP:USR:GIGE2,PORT=PS2,IMAGE_PORT=PS2Image,FILE_PORT=PS2File,ROI_PORT=PS2ROI,NDARRAY_PORT=PS2" )
+#
+# set debug flags
+###asynSetTraceMask("PS2",0,255)
+
 
 # Setup autosave
 set_savefile_path( "$(IOC_DATA)/$(IOC)/autosave" )
 set_requestfile_path( "$(TOP)/autosave" )
-save_restoreSet_status_prefix( "XPP:IOC:GIGE:02:" )
+save_restoreSet_status_prefix( "XPP:USR:GIGE2" )
 save_restoreSet_IncompleteSetsOk( 1 )
 save_restoreSet_DatedBackupFiles( 1 )
-set_pass0_restoreFile( "autosave_gige.sav" )
-set_pass1_restoreFile( "autosave_gige.sav" )
+set_pass0_restoreFile( "autosave_prosilica.sav" )
+set_pass1_restoreFile( "autosave_prosilica.sav" )
 
 # Initialize the IOC and start processing records
 iocInit()
 
+# FIXME - initialize these in the database
+dbpf "XPP:USR:GIGE2:cam1:DataType" UInt8
+dbpf "XPP:USR:GIGE2:cam1:SizeX" 1360
+dbpf "XPP:USR:GIGE2:cam1:SizeY" 1024
+dbpf "XPP:USR:GIGE2:image1:EnableCallbacks" 1
+
 # Start autosave backups
-create_monitor_set( "autosave_gige.req", 5, "IOC=XPP:IOC:GIGE:02" )
+create_monitor_set( "autosave_prosilica.req", 5, "P=XPP:USR:GIGE2" )
 
 # All IOCs should dump some common info after initial startup.
 < /reg/d/iocCommon/All/post_linux.cmd
