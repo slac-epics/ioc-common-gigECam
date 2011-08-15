03317588e085a031bd0db609453dfc86f2f3314b
Pavel Stoffel <pstoffel@slac.stanford.edu>
2011-08-15 21:43:17 +0000

    Made gige cam 2 similar to cam 1
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@7165 ececa60f-9f63-0410-97c7-ea05888eb260

--- iocBoot/ioc-xpp-gige-01/st.cmd
+++ iocBoot/ioc-xpp-gige-01/st.cmd
@@ -15,7 +15,7 @@ gige_registerRecordDeviceDriver(pdbbase)
 
 epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
 
-epicsEnvSet("PREFIX", "13PS1:")
+epicsEnvSet("PREFIX", "XPP:GIGE:")
 epicsEnvSet("PORT",   "PS1")
 epicsEnvSet("QSIZE",  "20")
 epicsEnvSet("XSIZE",  "1360")
@@ -32,19 +32,19 @@ prosilicaConfigIp(  "PS1", 116474, "192.168.100.220", 50, -1)
 asynSetTraceIOMask("$(PORT)",0,2)
 #asynSetTraceMask("$(PORT)",0,255)
 
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=CAM1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=CAM1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 # Note that prosilica.template must be loaded after NDFile.template to replace the file format correctly
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=CAM1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from first Prosilica driver.
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, -1)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=IMAGE1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 
 # Use this line if you only want to use the Prosilica in 8-bit mode.  It uses an 8-bit waveform record
 # NELEMENTS is set large enough for a 1360x1024x3 image size, which is the number of pixels in RGB images from the GC1380CH color camera. 
 # Must be at least as big as the maximum size of your camera images
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=4177920")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=IMAGE1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=4177920")
 
 #asynSetTraceMask("$(PORT)",0,255)
 
@@ -64,6 +64,9 @@ set_pass1_restoreFile( "autosave_gige.sav" )
 
 # Initialize the IOC and start processing records
 iocInit()
+
+dbpf $(PREFIX)CAM1:ArrayCallbacks 1
+dbpf $(PREFIX)IMAGE1:EnableCallbacks 1
 
 # Start autosave backups
 create_monitor_set( "autosave_gige.req", 5, "IOC=IOC:XPP:GIGE:01" )
--- iocBoot/ioc-xpp-gige-02/st.cmd
+++ iocBoot/ioc-xpp-gige-02/st.cmd
@@ -13,6 +13,42 @@ cd( "../.." )
 dbLoadDatabase("dbd/gige.dbd")
 gige_registerRecordDeviceDriver(pdbbase)
 
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
+
+epicsEnvSet("PREFIX", "XPP:GIGE:")
+epicsEnvSet("PORT",   "PS2")
+epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("XSIZE",  "1360")
+epicsEnvSet("YSIZE",  "1024")
+epicsEnvSet("NCHANS", "2048")
+
+##############################################################
+# configure and initialize the camera
+#   Args:  port, dummy, ip, nbufers, nbufers x width x height + overhead
+prosilicaConfigIp(  "PS2", 116474, "192.168.100.221", 50, -1)
+##############################################################
+
+
+asynSetTraceIOMask("$(PORT)",0,2)
+#asynSetTraceMask("$(PORT)",0,255)
+
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=CAM2:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=CAM2:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+# Note that prosilica.template must be loaded after NDFile.template to replace the file format correctly
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=CAM2:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+NDStdArraysConfigure("Image2", 5, 0, "$(PORT)", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=IMAGE2:,PORT=Image2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+
+# Use this line if you only want to use the Prosilica in 8-bit mode.  It uses an 8-bit waveform record
+# NELEMENTS is set large enough for a 1360x1024x3 image size, which is the number of pixels in RGB images from the GC1380CH color camera. 
+# Must be at least as big as the maximum size of your camera images
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=IMAGE2:,PORT=Image2,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=4177920")
+
+#asynSetTraceMask("$(PORT)",0,255)
+
+
 # Load record instances
 dbLoadRecords( "db/iocAdmin.db",			"IOC=IOC:XPP:GIGE:02" )
 dbLoadRecords( "db/save_restoreStatus.db",	"IOC=IOC:XPP:GIGE:02" )
@@ -28,6 +64,9 @@ set_pass1_restoreFile( "autosave_gige.sav" )
 
 # Initialize the IOC and start processing records
 iocInit()
+
+dbpf $(PREFIX)CAM2:ArrayCallbacks 1
+dbpf $(PREFIX)IMAGE2:EnableCallbacks 1
 
 # Start autosave backups
 create_monitor_set( "autosave_gige.req", 5, "IOC=IOC:XPP:GIGE:02" )
