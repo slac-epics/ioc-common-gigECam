ec6fdbdcb7ea004d9fa7eea897e5056ff6c80472
Pavel Stoffel <pstoffel@slac.stanford.edu>
2011-08-31 00:54:38 +0000

    Version that works with base 3.14.11-0.1.0
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@7298 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ iocBoot/ioc-tst-gige-01/st.cmd
@@ -0,0 +1,37 @@
+#! ../../bin/linux-x86_64/gige
+
+epicsEnvSet( "ENGINEER", "Pavel Stoffel (pstoffel)" )
+epicsEnvSet( "LOCATION", "TST:R40:IOC:18:GIGE:01" )
+epicsEnvSet( "IOCSH_PS1", "ioc-tst-gige-01> " )
+
+< envPaths
+cd( "../" )
+
+dbLoadDatabase("$(AREA_DETECTOR)/dbd/prosilicaApp.dbd")
+gige_registerRecordDeviceDriver(pdbbase)
+
+epicsEnvSet("PREFIX", "13PS1:")
+epicsEnvSet("PORT",   "PS1")
+epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("XSIZE",  "1360")
+epicsEnvSet("YSIZE",  "1024")
+epicsEnvSet("NCHANS", "2048")
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
+
+prosilicaConfigIp(  "PS1", 116474, "172.21.42.49", 50, -1)
+
+asynSetTraceIOMask("$(PORT)",0,2)
+
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=4177920")
+
+iocInit()
+
+dbpf $(PREFIX)cam1:ArrayCallbacks 1
+dbpf $(PREFIX)image1:EnableCallbacks 1
+
---
+++ iocBoot/ioc-tst-gige-01/startup.cmd
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Setup the IOC user environment
+source /reg/d/iocCommon/All/xpp_env.sh
+
+# Make sure the IOC's data directories are ready for use
+export IOC="ioc-xpp-gige-01"
+$RUNUSER "mkdir -p $IOC_DATA/$IOC/autosave"
+$RUNUSER "mkdir -p $IOC_DATA/$IOC/archive"
+$RUNUSER "mkdir -p $IOC_DATA/$IOC/iocInfo"
+
+# Make sure permissions are correct
+$RUNUSER "chmod ug+w -R $IOC_DATA/$IOC"
+
+# For release
+cd $EPICS_SITE_TOP/ioc/common/gige/R0.1.0/iocBoot/$IOC
+
+# Copy the archive file to iocData
+$RUNUSER "cp ../../archive/$IOC.archive $IOC_DATA/$IOC/archive"
+
+# Launch the IOC
+$RUNUSER "$PROCSERV --logfile $IOC_DATA/$IOC/iocInfo/ioc.log --name $IOC 32500 ./st.cmd"
