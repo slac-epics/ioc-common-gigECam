246d26f4ca378fb2d36ea1b76a7be8f112986ec7
Pavel Stoffel <pstoffel@slac.stanford.edu>
2012-02-06 22:33:59 +0000

    Configuration macros to support up to 4 cameras
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@8693 ececa60f-9f63-0410-97c7-ea05888eb260

--- iocBoot/ioc-tst-gige-01/st.cmd
+++ iocBoot/ioc-tst-gige-01/st.cmd
@@ -1,5 +1,5 @@
-#! /reg/g/pcds/package/epics/3.14/ioc/common/gigECam/R0.1.0/bin/linux-x86_64/gige
 #! ../../bin/linux-x86_64/gige
+#! /reg/g/pcds/package/epics/3.14/ioc/common/gigECam/R0.1.0/bin/linux-x86_64/gige
 
 # Run common startup commands for linux soft IOC's
 < /reg/d/iocCommon/All/pre_linux.cmd
@@ -10,84 +10,137 @@ epicsEnvSet( "ENGINEER", "Pavel Stoffel (pstoffel)" )
 epicsEnvSet( "LOCATION", "TST:R40:IOC:18:GIGE:01" )
 epicsEnvSet( "IOCSH_PS1", "ioc-tst-gige-01> " )
 
+epicsEnvSet("PREFIX", "TST:GIGE:")
+epicsEnvSet("CAM1",   "CAM1")
+epicsEnvSet("IMG1",   "IMAGE1")
+epicsEnvSet("CAM2",   "CAM2")
+epicsEnvSet("IMG2",   "IMAGE2")
+epicsEnvSet("CAM3",   "CAM3")
+epicsEnvSet("IMG3",   "IMAGE3")
+epicsEnvSet("CAM4",   "CAM4")
+epicsEnvSet("IMG4",   "IMAGE4")
+
+# ----- Manta G046B -----
+#epicsEnvSet("CAM1_ENABLED",  "")                             # "" = YES,  "#" = NO
+#epicsEnvSet("C1_IP",     "192.168.0.105")
+#epicsEnvSet("C1_XSIZE",  "780")
+#epicsEnvSet("C1_YSIZE",  "580")
+#epicsEnvSet("C1_COLORMODE",  "0")        # 0=Mono, 2=RGB1
+#epicsEnvSet("C1_NELEMENTS",  "452400")   # X * Y
+
+# ----- Manta G146C -----
+epicsEnvSet("CAM1_ENABLED",  "")                             # "" = YES,  "#" = NO
+epicsEnvSet("C1_IP",         "192.168.0.101")
+epicsEnvSet("C1_XSIZE",      "1388")
+epicsEnvSet("C1_YSIZE",      "1038")
+epicsEnvSet("C1_COLORMODE",  "2")        # 0=Mono, 2=RGB1
+epicsEnvSet("C1_NELEMENTS",  "4322232")  # X * Y * 3
+
+# ----- Manta G146C -----
+epicsEnvSet("CAM2_ENABLED",  "#")                             # "" = YES,  "#" = NO
+epicsEnvSet("C2_IP",         "192.168.0.105")
+epicsEnvSet("C2_XSIZE",      "1388")
+epicsEnvSet("C2_YSIZE",      "1038")
+epicsEnvSet("C2_COLORMODE",  "2")        # 0=Mono, 2=RGB1
+epicsEnvSet("C2_NELEMENTS",  "4322232")  # X * Y * 3
+
+# ----- Manta G146C -----
+epicsEnvSet("CAM3_ENABLED",  "#")                             # "" = YES,  "#" = NO
+epicsEnvSet("C3_IP",         "192.168.100.30")
+epicsEnvSet("C3_XSIZE",      "1388")
+epicsEnvSet("C3_YSIZE",      "1038")
+epicsEnvSet("C3_COLORMODE",  "2")        # 0=Mono, 2=RGB1
+epicsEnvSet("C3_NELEMENTS",  "4322232")  # X * Y * 3
+
+# ----- Manta G146C -----
+epicsEnvSet("CAM4_ENABLED",  "#")                             # "" = YES,  "#" = NO
+epicsEnvSet("C4_IP",         "192.168.100.40")
+epicsEnvSet("C4_XSIZE",      "1388")
+epicsEnvSet("C4_YSIZE",      "1038")
+epicsEnvSet("C4_COLORMODE",  "2")        # 0=Mono, 2=RGB1
+epicsEnvSet("C4_NELEMENTS",  "4322232")  # X * Y * 3
+
+#epicsEnvSet("XSIZE",  "1360")
+#epicsEnvSet("YSIZE",  "1024")
+
+# -----------------------
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
+
 cd( "../.." )
 
-epicsEnvSet("PREFIX", "TST:GIGE:01")
-epicsEnvSet("CAM1",   "CAM1")
-epicsEnvSet("CAM2",   "CAM2")
-epicsEnvSet("IMG1",   "IMAGE1")
-epicsEnvSet("IMG2",   "IMAGE2")
-#epicsEnvSet("PORT",   "PS1")
-epicsEnvSet("QSIZE",  "20")
-#epicsEnvSet("XSIZE",  "1360")
-#epicsEnvSet("YSIZE",  "1024")
-epicsEnvSet("XSIZE",  "1388")
-epicsEnvSet("YSIZE",  "1038")
-#epicsEnvSet("XSIZE",  "1360")
-#epicsEnvSet("YSIZE",  "1024")
-#epicsEnvSet("XSIZE",  "780")
-#epicsEnvSet("YSIZE",  "580")
-epicsEnvSet("NCHANS", "2048")
-epicsEnvSet("NELEMENTS1", "4177920")
-epicsEnvSet("NELEMENTS2", "4177920")
-#epicsEnvSet("NELEMENTS2", "452400")
-epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
-epicsEnvSet(  "IP1", "192.168.100.10" )
-epicsEnvSet(  "IP2", "192.168.0.101" )
-
 # Register all support components
-dbLoadDatabase( "dbd/gige.dbd" )
+dbLoadDatabase("dbd/gige.dbd")
 gige_registerRecordDeviceDriver(pdbbase)
 
 ##############################################################
 # configure and initialize the camera
 #   Args:  port, dummy, ip, nbufers, nbufers x width x height + overhead
-prosilicaConfigIp(  "PS1", 116474, "$(IP1)", 50, 120000000 )
-prosilicaConfigIp(  "PS2", 116474, "$(IP2)", 50, 120000000 )
+$(CAM1_ENABLED) prosilicaConfigIp(  "$(CAM1)", 999999, "$(C1_IP)", 50, -1)
+$(CAM1_ENABLED) epicsThreadSleep(1)
+$(CAM2_ENABLED) prosilicaConfigIp(  "$(CAM2)", 999999, "$(C2_IP)", 50, -1)
+$(CAM2_ENABLED) epicsThreadSleep(1)
+$(CAM3_ENABLED) prosilicaConfigIp(  "$(CAM3)", 999999, "$(C3_IP)", 50, -1)
+$(CAM3_ENABLED) epicsThreadSleep(1)
+$(CAM4_ENABLED) prosilicaConfigIp(  "$(CAM4)", 999999, "$(C4_IP)", 50, -1)
+$(CAM4_ENABLED) epicsThreadSleep(1)
+
 ##############################################################
 
+#asynSetTraceMask("$(CAM1)",0,9)
+#asynSetTraceIOMask("$(CAM1)",0,2)
+#asynSetTraceMask("$(CAM2)",0,9)
+#asynSetTraceIOMask("$(CAM2)",0,2)
+#asynSetTraceMask("$(CAM3)",0,9)
+#asynSetTraceIOMask("$(CAM3)",0,2)
+#asynSetTraceMask("$(CAM4)",0,9)
+#asynSetTraceIOMask("$(CAM4)",0,2)
 
-##asynSetTraceIOMask("PS1",0,2)
-#asynSetTraceMask("PS1",0,255)
-asynSetTraceIOMask("PS1",0,0)
-asynSetTraceIOMask("PS2",0,0)
-
-#asynSetTraceMask("PS1",0,255)
-asynSetTraceMask("PS1",0,0)
-asynSetTraceMask("PS2",0,0)
-
-
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=13PS1:,R=CAM1:,PORT=PS1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=13PS1:,R=CAM1:,PORT=PS1,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=13PS1:,R=CAM1:,PORT=PS1,ADDR=0,TIMEOUT=1")
-
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=13PS2:,R=CAM1:,PORT=PS2,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=13PS2:,R=CAM1:,PORT=PS2,ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=13PS2:,R=CAM1:,PORT=PS2,ADDR=0,TIMEOUT=1")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
+$(CAM2_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM2):,PORT=$(CAM2),ADDR=0,TIMEOUT=1")
+$(CAM2_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM2):,PORT=$(CAM2),ADDR=0,TIMEOUT=1")
+$(CAM2_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM2):,PORT=$(CAM2),ADDR=0,TIMEOUT=1")
+$(CAM3_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM3):,PORT=$(CAM3),ADDR=0,TIMEOUT=1")
+$(CAM3_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM3):,PORT=$(CAM3),ADDR=0,TIMEOUT=1")
+$(CAM3_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM3):,PORT=$(CAM3),ADDR=0,TIMEOUT=1")
+$(CAM4_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM4):,PORT=$(CAM4),ADDR=0,TIMEOUT=1")
+$(CAM4_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM4):,PORT=$(CAM4),ADDR=0,TIMEOUT=1")
+$(CAM4_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM4):,PORT=$(CAM4),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from first Prosilica driver.
-NDStdArraysConfigure("Image1", 5, 0, "PS1", 0, -1)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13PS1:,R=IMAGE1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS1,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13PS1:,R=IMAGE1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(NELEMENTS1)")
+$(CAM1_ENABLED) NDStdArraysConfigure("$(IMG1)", 5, 0, "$(CAM1)", 0, -1)
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=$(IMG1):,PORT=$(IMG1),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM1),NDARRAY_ADDR=0")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=$(IMG1):,PORT=$(IMG1),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(C1_NELEMENTS)")
 
-NDStdArraysConfigure("Image2", 5, 0, "PS2", 0, -1)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=13PS2:,R=IMAGE1:,PORT=Image2,ADDR=0,TIMEOUT=1,NDARRAY_PORT=PS2,NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=13PS2:,R=IMAGE1:,PORT=Image2,ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(NELEMENTS2)")
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+$(CAM2_ENABLED) NDStdArraysConfigure("$(IMG2)", 5, 0, "$(CAM2)", 0, -1)
+$(CAM2_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=$(IMG2):,PORT=$(IMG2),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM2),NDARRAY_ADDR=0")
+$(CAM2_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=$(IMG2):,PORT=$(IMG2),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(C2_NELEMENTS)")
 
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+$(CAM3_ENABLED) NDStdArraysConfigure("$(IMG3)", 5, 0, "$(CAM3)", 0, -1)
+$(CAM3_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=$(IMG3):,PORT=$(IMG3),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM3),NDARRAY_ADDR=0")
+$(CAM3_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=$(IMG3):,PORT=$(IMG3),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(C3_NELEMENTS)")
+
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+$(CAM4_ENABLED) NDStdArraysConfigure("$(IMG4)", 5, 0, "$(CAM4)", 0, -1)
+$(CAM4_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=$(IMG4):,PORT=$(IMG4),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM4),NDARRAY_ADDR=0")
+$(CAM4_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=$(IMG4):,PORT=$(IMG4),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(C4_NELEMENTS)")
 
 # Load record instances
-dbLoadRecords( "db/iocAdmin.db",			"IOC=TST:R40:IOC:18:GIGE:01" )
-dbLoadRecords( "db/save_restoreStatus.db",	"IOC=TST:R40:IOC:18:GIGE:01" )
-
+dbLoadRecords( "db/iocAdmin.db",			"IOC=$(LOCATION)" )
+dbLoadRecords( "db/save_restoreStatus.db",	"IOC=$(LOCATION)" )
 
 # Setup autosave
 set_savefile_path( "$(IOC_DATA)/$(IOC)/autosave" )
 set_requestfile_path( "autosave" )
-save_restoreSet_status_prefix("TST:R40:IOC:18:GIGE:01:")
+save_restoreSet_status_prefix("$(LOCATION)")
 save_restoreSet_IncompleteSetsOk( 1 )
 save_restoreSet_DatedBackupFiles( 1 )
-set_pass0_restoreFile( "tst_gige1.sav" )
-set_pass1_restoreFile( "tst_gige1.sav" )
+set_pass0_restoreFile( "$(IOC).sav" )
+set_pass1_restoreFile( "$(IOC).sav" )
 
 save_restoreSet_NumSeqFiles(5)
 save_restoreSet_SeqPeriodInSeconds(30)
@@ -95,14 +148,83 @@ save_restoreSet_SeqPeriodInSeconds(30)
 # Initialize the IOC and start processing records
 iocInit()
 
-dbpf 13PS1:CAM1:ArrayCallbacks 1
-dbpf 13PS1:IMAGE1:EnableCallbacks 1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):ArrayCallbacks 1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(IMG1):EnableCallbacks 1
+#
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):ColorMode $(C1_COLORMODE)         # 0=Mono, 2=RGB1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):DataType 0                        # 0=UInt8, 1=UInt16
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):ImageMode 2                       # 0=Single, 1=Multiple, 2=Continuous
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):TriggerMode 5                     # 0=Free Run, 1=SyncIn1, 5=Fixed Rate
+#
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):AcquirePeriod 1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):AcquireTime 0.1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):Gain 0
+#
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):Acquire 1                         # Start the camera
+#
+$(CAM1_ENABLED) dbpr $(PREFIX)$(CAM1):ArrayCounter_RBV
 
-dbpf 13PS2:CAM1:ArrayCallbacks 1
-dbpf 13PS2:IMAGE1:EnableCallbacks 1
+# ----------
+
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):ArrayCallbacks 1
+$(CAM2_ENABLED) dbpf $(PREFIX)$(IMG2):EnableCallbacks 1
+#
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):ColorMode $(C2_COLORMODE)         # 0=Mono, 2=RGB1
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):DataType 0                        # 0=UInt8, 1=UInt16
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):ImageMode 2                       # 0=Single, 1=Multiple, 2=Continuous
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):TriggerMode 5                     # 0=Free Run, 1=SyncIn1, 5=Fixed Rate
+#
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):AcquirePeriod 1
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):AcquireTime 0.1
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):Gain 0
+#
+$(CAM2_ENABLED) dbpf $(PREFIX)$(CAM2):Acquire 1                         # Start the camera
+#
+$(CAM2_ENABLED) dbpr $(PREFIX)$(CAM2):ArrayCounter_RBV
+
+# ----------
+
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):ArrayCallbacks 1
+$(CAM3_ENABLED) dbpf $(PREFIX)$(IMG3):EnableCallbacks 1
+#
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):ColorMode $(C3_COLORMODE)         # 0=Mono, 2=RGB1
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):DataType 0                        # 0=UInt8, 1=UInt16
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):ImageMode 2                       # 0=Single, 1=Multiple, 2=Continuous
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):TriggerMode 5                     # 0=Free Run, 1=SyncIn1, 5=Fixed Rate
+#
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):AcquirePeriod 1
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):AcquireTime 0.1
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):Gain 0
+#
+$(CAM3_ENABLED) dbpf $(PREFIX)$(CAM3):Acquire 1                         # Start the camera
+#
+$(CAM3_ENABLED) dbpr $(PREFIX)$(CAM3):ArrayCounter_RBV
+
+# ----------
+
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):ArrayCallbacks 1
+$(CAM4_ENABLED) dbpf $(PREFIX)$(IMG4):EnableCallbacks 1
+#
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):ColorMode $(C4_COLORMODE)         # 0=Mono, 2=RGB1
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):DataType 0                        # 0=UInt8, 1=UInt16
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):ImageMode 2                       # 0=Single, 1=Multiple, 2=Continuous
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):TriggerMode 5                     # 0=Free Run, 1=SyncIn1, 5=Fixed Rate
+#
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):AcquirePeriod 1
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):AcquireTime 0.1
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):Gain 0
+#
+$(CAM4_ENABLED) dbpf $(PREFIX)$(CAM4):Acquire 1                         # Start the camera
+#
+$(CAM4_ENABLED) dbpr $(PREFIX)$(CAM4):ArrayCounter_RBV
+
+# ----------
 
 # Start autosave backups
-create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM1),IMG=$(PREFIX)$(IMG1)")
+$(CAM1_ENABLED) create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM1),IMG=$(PREFIX)$(IMG1)")
+$(CAM2_ENABLED) create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM2),IMG=$(PREFIX)$(IMG2)")
+$(CAM3_ENABLED) create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM3),IMG=$(PREFIX)$(IMG3)")
+$(CAM4_ENABLED) create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM4),IMG=$(PREFIX)$(IMG4)")
 
 # All IOCs should dump some common info after initial startup.
 < /reg/d/iocCommon/All/post_linux.cmd
