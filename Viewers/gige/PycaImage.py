1018d2b027cec961a291efee10f40f64b523076d
Pavel Stoffel <pstoffel@slac.stanford.edu>
2012-02-16 21:37:45 +0000

    Added a FIXME comment
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@8788 ececa60f-9f63-0410-97c7-ea05888eb260

--- Viewers/gige/PycaImage.py
+++ Viewers/gige/PycaImage.py
@@ -63,6 +63,7 @@ class PycaImage():
                 colorTable = [QColor(i, i, i).rgb() for i in range(256)]
                 self.img.setColorTable(colorTable)
             # TODO  - emit changed img object signal if we had a previous img object
+            # FIXME - check img for None / make this thread safe
             buf = int(self.img.bits())
             size = w * h
             self.data_pv.set_processor(buf, size, format)
