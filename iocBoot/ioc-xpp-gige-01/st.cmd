ce8562f6193514a15d833d8477039a25a495dc1b
Silke Nelson <snelson@slac.stanford.edu>
2012-02-22 23:48:12 +0000

    minor changes to XPP gige IOCs
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@8827 ececa60f-9f63-0410-97c7-ea05888eb260

--- iocBoot/ioc-xpp-gige-01/st.cmd
+++ iocBoot/ioc-xpp-gige-01/st.cmd
@@ -1,75 +1,104 @@
 #! ../../bin/linux-x86_64/gige
 
-# Run common startup commands for linux soft IOC's
-< /reg/d/iocCommon/All/pre_linux.cmd
-
-< envPaths
-
 epicsEnvSet( "ENGINEER", "Pavel Stoffel (pstoffel)" )
 epicsEnvSet( "LOCATION", "IOC:XPP:GIGE:01" )
 epicsEnvSet( "IOCSH_PS1", "ioc-xpp-gige-01> " )
+< envPaths
+
+# Run common startup commands for linux soft IOC's
+< /reg/d/iocCommon/All/pre_linux.CD
+
+epicsEnvSet("PREFIX", "XPP:GIGE:")
+epicsEnvSet("CAM1",   "cam1")
+epicsEnvSet("IMG1",   "image1")
+epicsEnvSet("CAM3",   "CAM3")
+epicsEnvSet("IMG3",   "IMAGE3")
+
+##prosilica
+#epicsEnvSet("CAM1_ENABLED",  "")                             # "" = YES,  "#" = NO
+#epicsEnvSet("C1_IP",     "192.168.100.220")
+#epicsEnvSet("C1_XSIZE",  "1360")
+#epicsEnvSet("C1_YSIZE",  "1024")
+#epicsEnvSet("C1_COLORMODE",  "2")
+#epicsEnvSet("C1_NELEMENTS",  "4177920")  # X * Y * 3
+
+# ----- Manta G046B -----
+epicsEnvSet("CAM1_ENABLED",  "")                             # "" = YES,  "#" = NO
+epicsEnvSet("C1_IP",         "192.168.100.30")
+epicsEnvSet("C1_XSIZE",      "1388")
+epicsEnvSet("C1_YSIZE",      "1038")
+epicsEnvSet("C1_COLORMODE",  "2")        # 0=Mono, 2=RGB1
+epicsEnvSet("C1_NELEMENTS",  "4322232")  # X * Y * 3
+
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
+
 cd( "../.." )
 
 # Register all support components
 dbLoadDatabase( "dbd/gige.dbd" )
 gige_registerRecordDeviceDriver(pdbbase)
 
-
-epicsEnvSet("PREFIX", "XPP:GIGE:")
-epicsEnvSet("CAM1",   "cam1")
-epicsEnvSet("IMG1",   "image1")
-epicsEnvSet("XSIZE",  "1360")
-epicsEnvSet("YSIZE",  "1024")
-epicsEnvSet("NELEMENTS",  "4177920")  # X * Y * 3
-epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
-
 ##############################################################
 # configure and initialize the camera
 #   Args:  port, dummy, ip, nbufers, nbufers x width x height + overhead
-prosilicaConfigIp(  "$(CAM1)", 999999, "192.168.100.10", 50, -1)
+$(CAM1_ENABLED) prosilicaConfigIp(  "$(CAM1)", 999999, "$(C1_IP)", 50, -1)
+$(CAM1_ENABLED) epicsThreadSleep(1)
 ##############################################################
 
 
-##asynSetTraceIOMask("$(CAM1)",0,2)
-asynSetTraceIOMask("$(CAM1)",0,0)
+#asynSetTraceIOMask("$(CAM1)",0,2)
+#asynSetTraceIOMask("$(CAM1)",0,0)
 
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM1):,PORT=$(CAM1),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin, set it to get data from first Prosilica driver.
-NDStdArraysConfigure("$(IMG1)", 5, 0, "$(CAM1)", 0, -1)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=($IMG1):,PORT=$(IMG1),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM1),NDARRAY_ADDR=0")
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=($IMG1):,PORT=$(IMG1),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(NELEMENTS)")
+$(CAM1_ENABLED) NDStdArraysConfigure("$(IMG1)", 5, 0, "$(CAM1)", 0, -1)
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=$(IMG1):,PORT=$(IMG1),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM1),NDARRAY_ADDR=0")
+$(CAM1_ENABLED) dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=$(IMG1):,PORT=$(IMG1),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(C1_NELEMENTS)")
 
 #asynSetTraceMask("$(CAM1)",0,255)
-asynSetTraceMask("$(CAM1)",0,0)
-
+#asynSetTraceMask("$(CAM1)",0,0)
 
 # Load record instances
-dbLoadRecords( "db/iocAdmin.db",			"IOC=IOC:XPP:GIGE:01" )
+dbLoadRecords( "db/iocAdmin.db",		"IOC=IOC:XPP:GIGE:01" )
 dbLoadRecords( "db/save_restoreStatus.db",	"IOC=IOC:XPP:GIGE:01" )
 
-# Setup autosave
-##set_savefile_path( "$(IOC_DATA)/$(IOC)/autosave" )
-##set_requestfile_path( "autosave" )
-##save_restoreSet_status_prefix( "IOC:XPP:GIGE:01:" )
-##save_restoreSet_IncompleteSetsOk( 1 )
-##save_restoreSet_DatedBackupFiles( 1 )
-##set_pass0_restoreFile( "xpp_gige1.sav" )
-##set_pass1_restoreFile( "xpp_gige1.sav" )
+## Setup autosave
+set_savefile_path( "$(IOC_DATA)/$(IOC)/autosave" )
+set_requestfile_path( "$(TOP)/autosave" )
+save_restoreSet_status_prefix( "IOC:XPP:GIGE:01:" )
+save_restoreSet_IncompleteSetsOk( 1 )
+save_restoreSet_DatedBackupFiles( 1 )
+set_pass0_restoreFile( "xpp_gige1.sav" )
+set_pass1_restoreFile( "xpp_gige1.sav" )
 
-##save_restoreSet_NumSeqFiles(5)
-##save_restoreSet_SeqPeriodInSeconds(30)
+save_restoreSet_NumSeqFiles(5)
+save_restoreSet_SeqPeriodInSeconds(30)
 
 # Initialize the IOC and start processing records
 iocInit()
 
-dbpf $(PREFIX)$(CAM1):ArrayCallbacks 1
-dbpf $(PREFIX)($IMG1):EnableCallbacks 1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):ArrayCallbacks 1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(IMG1):EnableCallbacks 1
+#
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):ColorMode $(C1_COLORMODE)         # 0=Mono, 2=RGB1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):DataType 0                        # 0=UInt8, 1=UInt16
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):ImageMode 2                       # 0=Single, 1=Multiple, 2=Continuous
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):TriggerMode 5                     # 0=Free Run, 1=SyncIn1, 5=Fixed Rate
+#
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):AcquirePeriod 1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):AcquireTime 0.1
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):Gain 0
+#
+$(CAM1_ENABLED) dbpf $(PREFIX)$(CAM1):Acquire 1                         # Start the camera
+#
+$(CAM1_ENABLED) dbpr $(PREFIX)$(CAM1):ArrayCounter_RBV
 
-# Start autosave backups
-create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM1),IMG=$(PREFIX)$(IMG1)")
+## Start autosave backups
+$(CAM1_ENABLED) create_monitor_set("$(IOC).req", 5, "PRE=$(PREFIX),CAM=$(CAM1),IMG=$(IMG1)")
 
 # All IOCs should dump some common info after initial startup.
 < /reg/d/iocCommon/All/post_linux.cmd
