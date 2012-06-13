17e4413627b46ab2b007c65ddbc3148cc72d445a
Pavel Stoffel <pstoffel@slac.stanford.edu>
2012-06-13 20:45:06 +0000

    Two new cameras in MEC
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@9890 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ iocBoot/ioc-mec-las-gige8/st.cmd
@@ -0,0 +1,92 @@
+#! ../../bin/linux-x86_64/gige
+
+# Run common startup commands for linux soft IOC's
+< /reg/d/iocCommon/All/pre_linux.cmd
+
+< envPaths
+
+epicsEnvSet( "ENGINEER", "Pavel Stoffel (pstoffel)" )
+epicsEnvSet( "LOCATION",  "MEC:B999M64A:IOC:44" )
+epicsEnvSet( "IOC",       "ioc-mec-las-gige8")
+epicsEnvSet( "IOCSH_PS1", "$(IOC)> " )
+
+epicsEnvSet("PREFIX", "MEC:LAS:")
+epicsEnvSet("CAM",   "CAM8")
+epicsEnvSet("IMG",   "IMAGE8")
+
+# ----- Manta G-145B -----
+epicsEnvSet("C_IP",         "192.168.107.2")
+epicsEnvSet("C_XSIZE",      "780")
+epicsEnvSet("C_YSIZE",      "580")
+epicsEnvSet("C_COLORMODE",  "0")        # 0=Mono, 2=RGB1
+epicsEnvSet("C_NELEMENTS",  "452400")   # X * Y
+
+# -----------------------
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES", "8000000")
+
+cd( "../.." )
+
+# Register all support components
+dbLoadDatabase("dbd/gige.dbd")
+gige_registerRecordDeviceDriver(pdbbase)
+
+##############################################################
+# configure and initialize the camera
+#   Args:  port, dummy, ip, nbufers, nbufers x width x height + overhead
+prosilicaConfigIp(  "$(CAM)", 999999, "$(C_IP)", 50, -1)
+epicsThreadSleep(1)
+
+##############################################################
+
+#asynSetTraceMask("$(CAM)",0,9)
+#asynSetTraceIOMask("$(CAM)",0,2)
+
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=$(CAM):,PORT=$(CAM),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=$(CAM):,PORT=$(CAM),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/prosilica.template","P=$(PREFIX),R=$(CAM):,PORT=$(CAM),ADDR=0,TIMEOUT=1")
+
+# Create a standard arrays plugin, set it to get data from first Prosilica driver.
+NDStdArraysConfigure("$(IMG)", 5, 0, "$(CAM)", 0, -1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=$(IMG):,PORT=$(IMG),ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(CAM),NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=$(IMG):,PORT=$(IMG),ADDR=0,TIMEOUT=1,TYPE=Int8,FTVL=UCHAR,NELEMENTS=$(C_NELEMENTS)")
+
+# Load record instances
+dbLoadRecords( "db/iocAdmin.db",			"IOC=$(LOCATION)" )
+dbLoadRecords( "db/save_restoreStatus.db",	"IOC=$(LOCATION)" )
+
+# Setup autosave
+set_savefile_path( "$(IOC_DATA)/$(IOC)/autosave" )
+set_requestfile_path( "autosave" )
+save_restoreSet_status_prefix("$(LOCATION)")
+save_restoreSet_IncompleteSetsOk( 1 )
+save_restoreSet_DatedBackupFiles( 1 )
+set_pass0_restoreFile( "$(IOC).sav" )
+set_pass1_restoreFile( "$(IOC).sav" )
+
+save_restoreSet_NumSeqFiles(5)
+save_restoreSet_SeqPeriodInSeconds(30)
+
+# Initialize the IOC and start processing records
+iocInit()
+
+dbpf $(PREFIX)$(CAM):ArrayCallbacks 1
+dbpf $(PREFIX)$(IMG):EnableCallbacks 1
+#
+dbpf $(PREFIX)$(CAM):ColorMode $(C_COLORMODE)         # 0=Mono, 2=RGB1
+dbpf $(PREFIX)$(CAM):DataType 0                        # 0=UInt8, 1=UInt16
+dbpf $(PREFIX)$(CAM):ImageMode 2                       # 0=Single, 1=Multiple, 2=Continuous
+dbpf $(PREFIX)$(CAM):TriggerMode 5                     # 0=Free Run, 1=SyncIn1, 5=Fixed Rate
+#
+##dbpf $(PREFIX)$(CAM):AcquirePeriod 1
+##dbpf $(PREFIX)$(CAM):AcquireTime 0.1
+##dbpf $(PREFIX)$(CAM):Gain 0
+#
+##dbpf $(PREFIX)$(CAM):Acquire 1                         # Start the camera
+
+# ----------
+# Start autosave backups
+create_monitor_set("$(IOC).req", 5, "CAM=$(PREFIX)$(CAM),IMG=$(PREFIX)$(IMG)")
+
+# All IOCs should dump some common info after initial startup.
+< /reg/d/iocCommon/All/post_linux.cmd
