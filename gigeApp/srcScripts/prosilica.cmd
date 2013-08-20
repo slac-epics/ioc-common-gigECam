997c4e70fc1c60575e2719a6f9ed660eec66d307
Bruce Hill <bhill@slac.stanford.edu>
2013-08-20 03:25:52 +0000

    Moved all cmd and env scripts from iocBoot to gigeApp/srcScripts This should make it easier to separate the application from the deployment.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@13661 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ gigeApp/srcScripts/prosilica.cmd
@@ -0,0 +1,20 @@
+# Set the environment, configure, and load the DB
+# records for a Prosilica camera
+#
+# Each of these required macros must be re-defined
+# to unique values each time this script is invoked
+# Required macros:
+#	CAM			Prefix for all camera PV's
+#	CAM_IP		Network IP addr or hostname	for camera
+#	CAM_PORT	Asyn port name for camera
+
+# Configure and initialize a prosilica camera
+# prosilicaConfig( portName, ipName, maxBuffers, maxMemory, priority, stackSize, maxPvAPIFrames )
+# Set maxBuffers to 0 for unlimited buffers
+# Set maxMemory  to 0 for unlimited memory allocation
+# Set priority   to 0  for default priority
+# Set stackSize  to 0  for default stackSize
+# Set maxPvAPIFrames  to 0  for default (2 frame buffers)
+# Parameters not set explicitly are set to zero
+prosilicaConfig(  "$(CAM_PORT)", "$(CAM_IP)", 50 )
+dbLoadRecords( "db/prosilica.db",  "CAM=$(CAM),CAM_PORT=$(CAM_PORT)" )
