06a1528286b8da13cc83bc8521cc6b3db16977ca
Ernesto Paiser <paiser@slac.stanford.edu>
2014-10-23 17:35:00 +0000

    adding individual screens start scripts for laser alignment cameras 1-7 with the screens now added...
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@19250 ececa60f-9f63-0410-97c7-ea05888eb260

---
+++ edm-ioc-mec-las-gige02-1.cmd
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+# Setup edm environment
+export EPICS_HOST_ARCH=linux-x86
+source /reg/g/pcds/setup/epicsenv-3.14.9.sh
+export EPICS_CA_MAX_ARRAY_BYTES=9000000
+
+export IOC_PV=MEC:LAS:GIGE:IOC1
+export CAM=MEC:LAS:GIGE:CAM1
+export HUTCH=MEC
+export PVLIST=mec.lst
+edm -x -eolc	\
+	-m "IOC=${IOC_PV}"	\
+	-m "CAM=${CAM}"		\
+	-m "P=${CAM},R=:"	\
+	-m "HUTCH=${HUTCH}"	\
+	-m "PVLIST=${PVLIST}"	\
+	gigeScreens/gigeTop.edl  &
+
