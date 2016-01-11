f2aba829a7e11d0190cd4af5e0fe67397de2c7da
Abdullah Rashed Ahmed <apra@slac.stanford.edu>
2016-01-11 21:57:40 +0000

    Set lrm and hrm to include correct region size definitions
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@23682 ececa60f-9f63-0410-97c7-ea05888eb260

--- gigeScripts/HRAndColorGigeCam.py
+++ gigeScripts/HRAndColorGigeCam.py
@@ -13,6 +13,9 @@ for j in range(1, length):
 	caput(camName+":IMG_OVER2:EnableCallbacks", 1)
 	caput(camName+":Over2:EnableCallbacks", 0)
 	caput(camName+":ROI6:EnableCallbacks", 0)
+	caput(camName+":SizeX", 1936)         # Added this to correct the setup
+	caput(camName+":SizeY", 1456)         # script which uses a lower res cam
+
 #The following lines set the Frame Rate and Resolution of the camera
 	caput(camName+":AcquireTime",0.60)
 	caput(camName+":AcquirePeriod",0.033)
