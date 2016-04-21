86f43d7420dbe2ff5dfd9bdb52048df7ce2f2402
Bruce Hill <bhill@slac.stanford.edu>
2016-04-21 06:04:19 +0000

    Integrated netstat features into gigECam
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@25183 ececa60f-9f63-0410-97c7-ea05888eb260

--- gigeApp/src/netstat.c
+++ gigeApp/src/netstat.c
@@ -9,40 +9,30 @@
 #include <evrTime.h>              /* for EVR APIs                 */
 #include <epicsMutex.h>
 
-static int NET_COUNT = 0;
-
 #define MAX_NICS 16
-
-static FILE *tx_fp[MAX_NICS];
-static FILE *rx_fp[MAX_NICS];
-static time_t last_time;
-static unsigned long tx_bytes[MAX_NICS];
-static unsigned long rx_bytes[MAX_NICS];
-static unsigned long tx_rate[MAX_NICS];
-static unsigned long rx_rate[MAX_NICS];
 
 #define NIC_ID_SIZE 10
 
-typedef struct {
+typedef struct
+{
   char nic[NIC_ID_SIZE];
-  double tx_rate; /* in Mbps */
-  double rx_rate; /* in Mbps */
+  double tx_rate; /* in MBps */
+  double rx_rate; /* in MBps */
 } nic_stats_t;
 
 static int num_nics = 0;
 
 static nic_stats_t nic_stats[MAX_NICS];
 
-static epicsMutexId mutex;
+static epicsMutexId     netstatMutex    = 0;
 
-static void netstatThread() {
+static void * netstatThread( void * pUnused )
+{
   FILE *fp;
   char line[1400];
   char interface[20];
-  /* This is the RHEL5 command: */
-  /*  char *cmd="/usr/bin/sar -n DEV 1 | grep Average | grep -v IFACE | tr -s \" \" | cut -d\" \" -f 2,5,6"; */
-  /* This is the RHEL6 command: */
-  char *cmd="/usr/bin/sar -n DEV 1 1 | grep Average | grep -v IFACE | tr -s \" \" | cut -d\" \" -f 2,5,6";
+  /* This is the sar command: */
+  char *cmd="/usr/bin/sar -n DEV 1 1 | grep Average | tr -s \" \" | cut -d\" \" -f 2,5,6";
   double rx_rate;
   double tx_rate;
 
@@ -53,55 +43,72 @@ static void netstatThread() {
       printf("ERROR: fail on popen\n");
     }
     else {
-      epicsMutexLock(mutex);
+      epicsMutexLock(netstatMutex);
+      double rateDiv    = 1e3;
       int if_counter = 0;
       while (fgets(line, sizeof(line)-1, fp) != NULL) {
-/* 	printf("%s", line); */
-	if (if_counter < MAX_NICS) {
-	  sscanf(line, "%s %lf %lf", interface, &rx_rate, &tx_rate);
-	  strcpy(nic_stats[if_counter].nic, interface);
-	  nic_stats[if_counter].tx_rate = tx_rate * 8 / 1024;
-	  nic_stats[if_counter].rx_rate = rx_rate * 8 / 1024;
-	  /*
-	  printf("%s %lf %lf\n", nic_stats[if_counter].nic, 
-		 nic_stats[if_counter].tx_rate,
-		 nic_stats[if_counter].rx_rate);
-	  */
-	}
-	if_counter++;
+/*      printf("%s", line); */
+        if ( strstr( line, "rxbyt/s" ) ) {
+            rateDiv = 1e6;
+            continue;
+        }
+        if ( strstr( line, "rxkB/s" ) ) {
+            rateDiv = 1e3;
+            continue;
+        }
+        if ( sscanf(line, "%s %lf %lf", interface, &rx_rate, &tx_rate) != 3 )
+            continue;
+        if (if_counter < MAX_NICS) {
+          strcpy(nic_stats[if_counter].nic, interface);
+          nic_stats[if_counter].tx_rate = tx_rate / rateDiv;
+          nic_stats[if_counter].rx_rate = rx_rate / rateDiv;
+          /*
+          printf("%s %lf %lf\n", nic_stats[if_counter].nic, 
+                 nic_stats[if_counter].tx_rate,
+                 nic_stats[if_counter].rx_rate);
+          */
+          if_counter++;
+        }
       }
       num_nics = if_counter;
-      epicsMutexUnlock(mutex);
+      epicsMutexUnlock(netstatMutex);
       pclose(fp);
     }
 
     sleep(3);
   }
+  return NULL;
 }
 
-pthread_t netstatThreadId;
+static pthread_t netstatThreadId;
 
-static long subNetstatInit(subRecord *prec) {
-  mutex = epicsMutexMustCreate();
-
-  pthread_create(&netstatThreadId, NULL, netstatThread, NULL);
+static long subNetstatInit(subRecord *prec)
+{
+        if ( netstatMutex == 0 )
+        {
+                netstatMutex = epicsMutexMustCreate();
+                if ( netstatMutex == 0 )
+                {
+                        printf( "Error creating netstatMutex!" );
+                        return -1;
+                }
+                pthread_create(&netstatThreadId, NULL, netstatThread, NULL);
+        }
 
   return 0;
 }
 
-static long subNetstat(subRecord *prec) {
+static long subNetstat(subRecord *prec)
+{
   evrTimeGet(&(prec->time), 0);
   prec->val = 0;
-
-  time_t now = time(0);
-  unsigned long bytes_now;
 
   // If NIC exists, provide the current rate
   unsigned int nic_index = prec->a;
   unsigned int function = prec->b;
 
-  epicsMutexLock(mutex);
-  if (nic_index < MAX_NICS && nic_index < num_nics != NULL) {
+  epicsMutexLock(netstatMutex);
+  if ( nic_index < MAX_NICS && nic_index < num_nics ) {
     // B == 0 -> TX
     if (function == 0) {
       prec->val = nic_stats[nic_index].tx_rate;
@@ -112,11 +119,11 @@ static long subNetstat(subRecord *prec) {
   }
   /*
   printf("%d %d %s %lf %lf\n", nic_index, function, nic_stats[nic_index].nic, 
-	 nic_stats[nic_index].tx_rate,
-	 nic_stats[nic_index].rx_rate);
+         nic_stats[nic_index].tx_rate,
+         nic_stats[nic_index].rx_rate);
   */
 
-  epicsMutexUnlock(mutex);
+  epicsMutexUnlock(netstatMutex);
   return 0;  
 }
 
