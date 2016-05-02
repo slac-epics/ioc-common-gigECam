007a1392584d360faa126cee0795ebea01baf8ec
Abdullah Rashed Ahmed <apra@slac.stanford.edu>
2016-05-02 19:21:57 +0000

    Cleaned up setupgigecam
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@25308 ececa60f-9f63-0410-97c7-ea05888eb260

--- gigeScripts/SetupGigeCam.py
+++ gigeScripts/SetupGigeCam.py
@@ -1,28 +1,27 @@
-# gigECam Setup Script
 """
 SetupGigeCam.py
 
 Usage:
   SetupGigeCam.py <PV>... [--config=config] [-v|--verbose] [-z|--zenity]
-  SetupGigeCam.py <PV>... [--hutch=hutch] [--extra=extra] [-v|--verbose] [-z|--zenity] [--HR|--LR]
+  SetupGigeCam.py <PV>... [--hutch=hutch] [-v|--verbose] [-z|--zenity] [--HR|--LR] [--extra=extra] 
   SetupGigeCam.py [-h|--help]
 
 Arguments:
   <PV>                Camera PV. To do multiple cameras, input the full PV as 
                       the first argument, and then the numbers of the rest as 
-                      single entries, or in a range using a hyphen. An example 
-                      would be the following: 
+                      single entries, or in a range using a hyphen. Ex:
 
                           python SetupGigeCam.py SXR:EXP:GIGE:01-03 05
 
                       This will set up gigecams 01, 02, 03, and 05
 
 Options:
-  -h|--help           Show this help message
+  -h,--help           Show this help message
   --config=cfg_name   Explicitly specify which cfg to use.
   --hutch             Explicitly specify which hutch to use
-  -v|--verbose        Print more about active process
-  -z|--zenity         Outputs messages via zenity
+  --extra=extra       Extra string to pad end of config path
+  -v,--verbose        Print more about active process
+  -z,--zenity         Outputs messages via zenity
   --HR                Use high res mode for cfg
   --LR                Use low res mode for cfg
 
@@ -30,14 +29,13 @@ General quick setup for the gigECams. To create a new configuration just create
 a new cfg file in the configurations folder and then run the script specifying
 the config name to use it.
 """
-import pyca
-import os.path
+
+import os
 import sys
 from psp.Pv import Pv
 from ConfigParser import SafeConfigParser
 from optparse import OptionParser
 from pprint import pprint
-from os import system
 
 # caput is defined identically but with a timeout value of 10.0 instead of 2.0
 # The default setting of 2.0 seconds was too short, causing the script to fail
@@ -59,12 +57,12 @@ def SetupGigeCamera(camName, config, verbose, zenity):
 	# Perform the caputs
 	nFailedCaputs, failedCaputs, nCaputs = runCaputs(parser, camName, verbose, zenity)
 	# Display any failures
-	print "\nSetup complete for {0}".format(camName)
+	print "Setup complete for {0}".format(camName)
 	if nFailedCaputs > 0:
 		print "\nNumber of failed caputs: {0} / {1}".format(nFailedCaputs, nCaputs)
 		print "PVs that failed:"
 		pprint(failedCaputs)
-	if zenity: system('zenity --info --text="Completed Setup"')
+	if zenity: os.system('zenity --info --text="Completed Setup"')
 
 def runCaputs(parser, camName, verbose, zenity):
 	""" 
@@ -109,14 +107,14 @@ def getParser(config, verbose, zenity):
 	try: parser = SafeConfigParser()
 	except:
 		if zenity: 
-			system("zenity --error --text='Error: Failed to create parser'")
+			os.system("zenity --error --text='Error: Failed to create parser'")
 		print "Failed to create parser object."
 		return None
 	parser.optionxform=str				# Maintain case sensitivity
 	try: parser.read(config)
 	except:
 		if zenity:
-			system("zenity --error --text='Error: Failed to read config'")
+			os.system("zenity --error --text='Error: Failed to read config'")
 		print "Failed to read from configuration file."
 		return None	
 	return parser
@@ -128,21 +126,24 @@ def getConfig(PV, verbose, **kwargs):
 	else:
 		hutch = PV[:3]
 		if hutch.lower() == "sxr" or hutch.lower() == "amo":
-			hutch = "sxd"	
-	col, hr, mode, extra = "", "", "", ""
-	if 'HR' in kwargs and kwargs['HR']:
-		mode = "_hr_mode"
-	elif 'LR' in kwargs and kwargs['LR']:
-		mode = "_lr_mode"
+			hutch = "sxd"
+	col, hr, mode = "", "", ""
+	if 'HR' in kwargs and kwargs['HR']: mode = "_hr_mode"
+	elif 'LR' in kwargs and kwargs['LR']: mode = "_lr_mode"
 	else:
 		if ":col:" in PV.lower(): col = "_col"
 		else: col = ""
 		if ":hr:" in PV.lower() : hr = "_hr"
-		else: hr = ""	
+		else: hr = ""
+	cfgName = hutch + hr + col + mode
+	## Add new parameters below
+	##
+	extra = ""
 	if 'extra' in kwargs and kwargs['extra']:
 		extra = "_{0}".format(kwargs['extra'])
-	cfgName = hutch + hr + col + mode + extra
+	cfgName += extra
 	config = "./gigeScripts/configurations/gige_{0}.cfg".format(cfgName)
+	##
 	# Make sure the file exists
 	if verbose: print "Checking config file"
 	if not os.path.isfile(config):
@@ -152,28 +153,35 @@ def getConfig(PV, verbose, **kwargs):
 		else: config = config[0:2] + config[14:]
 	return config
 
+def getBasePV(PVArguments):
+	"""Returns the first base PV found in the list of PVArguments"""
+	for arg in PVArguments:
+		if ':' not in arg: continue
+		for i, char in enumerate(arg[::-1]):
+			if char == ':':
+				return arg[:-i]
+	return None
+
 def parsePVArguments(PVArguments):
 	"""Parses PV input arguments and returns a set of cam PVs"""
 	camPVs = set()
-	if len(PVArguments) == 0: return
-	if '-' in PVArguments[0]:
-		basePV = PVArguments[0].split('-')[0][:-2]
-	else:
-		basePV = PVArguments[0][:-2]
+	if len(PVArguments) == 0: return None
+	basePV = getBasePV(PVArguments)
+	if not basePV: return None
 	for arg in PVArguments:
 		try:
 			if '-' in arg:
 				splitArgs = arg.split('-')
-				if splitArgs[0][:-2] == basePV: camPVs.add(splitArgs[0])
+				if getBasePV(splitArgs[0]) == basePV: camPVs.add(splitArgs[0])
 				start = int(splitArgs[0][-2:])
 				end = int(splitArgs[1])
 				while start <= end:
-					camPVs.add(basePV + "%02d"%start)
+					camPVs.add(basePV + "{:02}".format(start))
 					start += 1
 			elif len(arg) > 3:
-				if arg[:-2] == basePV: camPVs.add(arg)
+				if getBasePV(arg) == basePV: camPVs.add(arg)
 			elif len(arg) < 3:
-				camPVs.add(basePV + "%02d"%int(arg))
+				camPVs.add(basePV + "{:02}".format(int(arg)))
 			else: pass
 		except: pass
 	camPVs = list(camPVs)
@@ -182,7 +190,7 @@ def parsePVArguments(PVArguments):
 
 
 if __name__ == "__main__":
-	parser = OptionParser(add_help_option=False)
+	parser = OptionParser(add_help_option=False)   #Disable help to use docstring
 	parser.add_option('--config', action='store', type='string', dest='config')
 	parser.add_option('--hutch', action='store', type='string', dest='hutch', default=None)
 	parser.add_option('--extra', action='store', type='string', dest='extra', default=None)
@@ -222,6 +230,5 @@ if __name__ == "__main__":
 	if config:
 		if verbose: print "Using config file: {0}".format(config)
 		for camName in camPVs:
-			print "Setting up {0}".format(camName)
-			SetupGigeCamera(camName, config, verbose, zenity)
-	
+			print "\nSetting up {0}".format(camName)
+			SetupGigeCamera(camName, config, verbose, zenity)	
