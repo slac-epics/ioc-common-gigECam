0c363d5b14c23913fb2d63038ee57b735496c05f
Bruce Hill <bhill@slac.stanford.edu>
2016-07-01 06:54:24 +0000

    Updated to ADStreamScreens R1.5.0 and added support for SCREENS_TOP in cfg files. Also fixed some minor st.cmd issues and added EDM_TOP to generated edm launcher.
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@26036 ececa60f-9f63-0410-97c7-ea05888eb260

--- iocBoot/templates/edm-ioc.cmd
+++ iocBoot/templates/edm-ioc.cmd
@@ -14,18 +14,26 @@ export EVR_PV=$$IF(EVR_PV,$$EVR_PV,NoEvr)
 export IOC_PV=$$IOC_PV
 export CAM=$$CAM_PV
 export TRIG_CH=$$IF(EVR_TRIG,$$EVR_TRIG,0)
-export HUTCH=$$HUTCH
+export HUTCH=$$IF(HUTCH,$$HUTCH,tst)
 
 export IF=$$IF(NET_IF,$$NET_IF,ETH0)
 
-pushd ${EPICS_SITE_TOP}-dev/screens/edm/${HUTCH}/current
+EDM_TOP=gigeScreens/gigeTop.edl
+$$IF(SCREENS_TOP)
+SCREENS_TOP=$$SCREENS_TOP
+$$ELSE(SCREENS_TOP)
+SCREENS_TOP=${EPICS_SITE_TOP}-dev/screens/edm/${HUTCH}/current
+$$ENDIF(SCREENS_TOP)
+
+pushd ${SCREENS_TOP}
 edm -x -eolc	\
-	-m "IOC=${IOC_PV}"	\
-	-m "EVR=${EVR_PV}"	\
-	-m "CAM=${CAM}"		\
-	-m "CH=${TRIG_CH}"	\
-	-m "P=${CAM},R=:"	\
-	-m "HUTCH=${HUTCH}"	\
-	-m "IF=${IF}"		\
-	gigeScreens/gigeTop.edl  &
+	-m "IOC=${IOC_PV}"		\
+	-m "EVR=${EVR_PV}"		\
+	-m "CAM=${CAM}"			\
+	-m "CH=${TRIG_CH}"		\
+	-m "P=${CAM},R=:"		\
+	-m "EDM_TOP=${EDM_TOP}"	\
+	-m "HUTCH=${HUTCH}"		\
+	-m "IF=${IF}"			\
+	${EDM_TOP} &
 
