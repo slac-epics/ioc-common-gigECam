a2635a578a1084f3b69cea39a8f7cd583fc01d88
Bruce Hill <bhill@slac.stanford.edu>
2016-11-17 07:18:12 +0000

    Modified build files and ifdef's to allow building w/ or w/o event2 or RULES.ioc-targets
    git-svn-id: file:///afs/slac/g/pcds/vol2/svn/pcds/epics/trunk/ioc/common/gigECam/current@27282 ececa60f-9f63-0410-97c7-ea05888eb260

--- gigeApp/src/gigeMain.cpp
+++ gigeApp/src/gigeMain.cpp
@@ -9,14 +9,18 @@
 
 #include "epicsExit.h"
 #include "epicsThread.h"
+#ifdef USE_EVR_IRQ_HANDLER
 #include "evrIrqHandler.h"
+#endif
 #include "iocsh.h"
 
 int main(int argc,char *argv[])
 {
 
+#ifdef USE_EVR_IRQ_HANDLER
 	// EVR module must block SIGIO from the main process
 	evrIrqHandlerInit();
+#endif
 
     if(argc>=2) {    
         iocsh(argv[1]);
